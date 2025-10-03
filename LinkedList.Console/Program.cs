2524b95c84edf122c6b2b2865bd3bac8a18bd321
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2025-10-03 15:10:55 -0500

    Basic example for single list

---
+++ LinkedList.Console/Program.cs
@@ -0,0 +1,43 @@
+﻿using LinkedList.Core;
+using System.ComponentModel.Design;
+
+var list = new SinglyLinkedList<string>();
+var opt = string.Empty;
+do 
+{
+    opt = Menu();
+    switch(opt)
+    {
+        case "0":
+            Console.WriteLine("Bye");
+            break;
+        case "1":
+            Console.Write("Enter value: ");
+            list.InsertAtBeginning(Console.ReadLine()!);
+            break;
+        case "2":
+            Console.Write("Enter value: ");
+            list.InsertAtEnd(Console.ReadLine()!);
+            break;
+        case "3":
+            list.PrintList();
+            break;
+        default:
+            Console.WriteLine("Invalid option");
+            break;
+    }
+    
+    
+} while (opt != "0");
+
+string Menu()
+{
+    Console.WriteLine(":::::::: Menu ::::::::");
+    Console.WriteLine();
+    Console.WriteLine("1. Insert at beginning");
+    Console.WriteLine("2. Insert at end");
+    Console.WriteLine("3. Print list");
+    Console.WriteLine("0. Exit");
+    Console.Write("Choose an option: ");
+    return Console.ReadLine()!;
+}
