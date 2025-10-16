f85f4e1ebcf6a2e335d59662f684bef8e1d3b5c6
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2025-10-15 19:49:48 -0500

    Updates

---
+++ DoubleList/DoublyLinkedList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleList;
+
+public class DoublyLinkedList<T>
+{
+    private DoubleNode<T>? _head;
+    private DoubleNode<T>? _tail;
+
+    public DoublyLinkedList()
+    {
+        _tail = null;
+        _head = null;
+    }
+
+    public void InsertAtBeginning(T data)
+    {
+        var newNode = new DoubleNode<T>(data);
+        if (_head == null)  //Empty list
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+            newNode.Next = _head;
+            _head.Prev = newNode;
+            _head = newNode;
+        }
+    }
+
+    public void InsertAtEnd(T data)
+    {
+        var newNode = new DoubleNode<T>(data);
+        if (_tail == null)  
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+            newNode.Prev = _tail;
+            _tail!.Next = newNode;
+            _tail = newNode;
+        }
+    }
+
+    public void PrintForward()
+    {
+
+    }
+
+}
